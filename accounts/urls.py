68787cb5a3b93d0a7c53b4cafcf0010db04322d8
Adeleke Femi <oluwafemiadeleke13@gmail.com>
2023-05-05 12:59:12 +0100

    reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('djoser/', include('djoser.urls')),
     path('admin/users', views.GetAdminStaffView.as_view()),
     path('superadmin/admins', views.GetSuperUserAdmins.as_view()),
-    path('reset/user/password', views.AdminResetPassword.as_view())
+    path('reset/user/password/<int:pk>', views.AdminResetPassword.as_view())
     
 ]
