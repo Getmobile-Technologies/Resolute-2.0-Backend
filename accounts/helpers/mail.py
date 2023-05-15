f88b253634f5f2c419032a903b1287a54f7fb9c9
Adeleke Femi <oluwafemiadeleke13@gmail.com>
2023-05-15 16:52:44 +0100

    fix

--- accounts/helpers/mail.py
+++ accounts/helpers/mail.py
@@ -13,8 +13,8 @@ def signup_mail(email, password, first_name):
         },
         json={
             "event": "sign",
+            "email": email,
             "data": {
-                "email": email,
                 "password": password,
                 "url": url,
                 "first_name": first_name
