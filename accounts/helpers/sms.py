b12acc417b9418fd854ca82473c7b8ca69a5103f
Adeleke Femi <oluwafemiadeleke13@gmail.com>
2023-05-18 13:27:05 +0100

    sms

--- accounts/helpers/sms.py
+++ accounts/helpers/sms.py
@@ -1,13 +1,15 @@
 import nexmo
+import vonage
 import os
 
 Vonage_API_Key = os.getenv("vonage_api_key")
 Vonage_API_Secret = os.getenv("vonage_secret_key")
 
 
-nexmo_client = nexmo.Client(
+nexmo_client = vonage.Client(
     key=Vonage_API_Key, secret=Vonage_API_Secret
 )
+sms = vonage.Sms(nexmo_client)
 Vonage_number = os.getenv("vonage_number")
 
 def sign_up_sms(number, pin):
@@ -18,12 +20,11 @@ def sign_up_sms(number, pin):
     PIN: {pin}
     Thank you!.
     """
-    request = nexmo_client.send_message({
-        "from": Vonage_number,
+    request = sms.send_message({
+        "from": "Resolute",
         "to": number,
-        "message": message
+        "text": message
     })
-    print(request)
 
     return request
     
