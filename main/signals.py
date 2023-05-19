f2dae5b1c8d2fa29d197601b2ad0eb2ba387eca4
Adeleke Femi <oluwafemiadeleke13@gmail.com>
2023-05-19 17:33:25 +0100

    sms

--- main/signals.py
+++ main/signals.py
@@ -9,7 +9,7 @@ from accounts.helpers.sms import emergency_sms
 
 
 @receiver(post_save, sender=PanicRequest)
-def send_emergency_sms(sender, instance, created, **kwargs):
+def send_emergency_sms(instance, created, **kwargs):
     if created:
         try:
             contacts = EmergencyContact.objects.all()
