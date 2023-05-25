ca6b8647bc649e2c563b104f77cb781978bf30d3
Adeleke Femi <oluwafemiadeleke13@gmail.com>
2023-05-25 09:50:45 +0100

    admin register

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import PanicRequest, CallRequest, TrackMeRequest, StaffLocation, Images, Notifications, EmergencyContact
 
 # Register your models here.
+
+
+admin.site.register([
+    PanicRequest,
+    CallRequest,
+    TrackMeRequest,
+    StaffLocation,
+    Images,
+    Notifications,
+    EmergencyContact
+])
+
