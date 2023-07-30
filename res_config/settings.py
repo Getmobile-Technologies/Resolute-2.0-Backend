e048e35d2087f991d28cbacc319abe929861c6d4
sirdesmond09 <sirdesmond09@gmail.com>
2023-07-30 23:32:51 +0100

    fix auth

--- res_config/settings.py
+++ res_config/settings.py
@@ -252,7 +252,7 @@ REST_FRAMEWORK = {
 
 
 AUTHENTICATION_BACKENDS = [
-        'accounts.authentication.PhoneNumberBackend'
+        'accounts.authentication.PhoneNumberBackend',
         'accounts.authentication.EmailBackend',
         'django.contrib.auth.backends.ModelBackend',
         'django.contrib.auth.backends.AllowAllUsersModelBackend',
